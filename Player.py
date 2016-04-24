23ab7db1e5b5a7957a10710f36859b0f3cdf6491
marissa <mxb9328@g.rit.edu>
2016-04-24 17:55:04 -0400

    finished main monster super class and one of the subclasses

--- Player.py
+++ Player.py
@@ -35,9 +35,43 @@ class Player:
         return self.level
 
 #Creates a player and prints out their initial attributes
+
+class Monster:
+    def __init__(self, name):
+        self.name = name
+        self.hp = 50
+        self.attack = 2
+        self.level = 1
+
+    def getName(self):
+         return self.name
+
+    def getHp(self):
+        return self.hp
+
+    def getAttack(self):
+        return self.attack
+
+class FracMonster(Monster):
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = "FracMonster"
+
+
+
+
+
+
+
 p1 = Player("Bob")
 print("Player's name: " +p1.getName())
 print("Player's hit point: " , p1.getHp())
 print("Player's attack points: " , p1.getAttack())
 print("Player's Level: " , p1.getLevel())
 
+m1 = FracMonster("Fractions")
+m1.hp = 100
+m1.attack = 3
+print("Monster's name: " + m1.getName())
+print("Monster's HP: " , m1.getHp())
+print("Monster's attack stat: ", m1.getAttack())
